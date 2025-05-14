5e4cd199cff41eb269afe05f373e3f24587a5df5
YuboxtY <juanpz2003@gmail.com>
2025-05-14 12:53:28 -0500

    Inicio del proyecto

---
+++ src/main/java/SistemaDeViajes/sistema/SistemadeviajesApplication.java
@@ -0,0 +1,13 @@
+package SistemaDeViajes.sistema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemadeviajesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemadeviajesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SistemaDeViajes/sistema/SistemadeviajesApplicationTests.java
@@ -0,0 +1,13 @@
+package SistemaDeViajes.sistema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemadeviajesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
