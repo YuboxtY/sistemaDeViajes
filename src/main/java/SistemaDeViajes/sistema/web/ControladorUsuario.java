bc1beb8e199597262d9ea9677af410cabe10cd09
Carryd_piedra <diegocarrion972@gmail.com>
2025-05-23 17:07:33 -0500

    Segundo Intento

--- src/main/java/SistemaDeViajes/sistema/web/ControladorUsuario.java
+++ src/main/java/SistemaDeViajes/sistema/web/ControladorUsuario.java
@@ -9,9 +9,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @Slf4j
+@RequestMapping("/usuario")
 public class ControladorUsuario {
     @Autowired
     private UsuarioService usuarioService;
