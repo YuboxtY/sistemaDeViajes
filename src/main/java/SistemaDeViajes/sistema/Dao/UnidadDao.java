e39f3f55b7ee35d42b01c2e0d1d72abb8de0d41f
YuboxtY <juanpz2003@gmail.com>
2025-05-23 20:10:13 -0500

    Se implemento las relaciones ManyToMany entre Rutas y Unidades, tambien la interfaz del programa, modificaciones en los Services y Controllers

--- src/main/java/SistemaDeViajes/sistema/Dao/UnidadDao.java
+++ src/main/java/SistemaDeViajes/sistema/Dao/UnidadDao.java
@@ -1,9 +1,15 @@
-package SistemaDeViajes.sistema.Dao;
+package SistemaDeViajes.sistema.dao;
 
 import SistemaDeViajes.sistema.Domain.Unidad;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UnidadDao extends CrudRepository <Unidad, String> { // Unidad es la entidad y String es el tipo de dato del ID
+import java.util.List;
+
+public interface UnidadDao extends CrudRepository <Unidad, String> {
+
+    List<Unidad> findByEstado(Unidad.estadoUnidad estado);
+
+    // Unidad es la entidad y String es el tipo de dato del ID
     // No es necesario agregar métodos adicionales, ya que CrudRepository proporciona métodos básicos como save(), findById(), findAll(), deleteById(), etc.
     // Puedes agregar métodos personalizados si es necesario, pero para operaciones CRUD básicas, no es necesario.
 
